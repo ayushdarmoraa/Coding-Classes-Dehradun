59c173ca51f7de88c43a6c16199d8c8298f9a484
ayushdarmoraa <ayushdarmoraa@gmail.com>
2025-08-31 21:43:28 -0700

    Add testimonials page

---
+++ src/app/testimonials/page.tsx
@@ -0,0 +1,8 @@
+export default function TestimonialsPage() {
+  return (
+    <div className="space-y-4">
+      <h1 className="text-3xl font-bold">Testimonials</h1>
+      <p>Our students love learning with us! We'll be adding testimonials soon.</p>
+    </div>
+  )
+}
